d100814fd423a382078cc3c449b56117a14027d1
riverojsx <dvxjsx@gmail.com>
2026-09-06 00:06:28 -0500

    Update script.js

--- frontend/script.js
+++ frontend/script.js
@@ -7,7 +7,7 @@ const profileBtn = document.getElementById("profile-btn");
 const profileMenu = document.getElementById("profile-menu");
 const profileUsername = document.getElementById("profile-username");
 
-profileUsername.textContent = "currentUser";
+profileUsername.textContent = currentUser;
 profileBtn.addEventListener("click", function() {profileMenu.style.display = "block";});
 const closeProfileBtn = document.getElementById("close-profile-btn"); closeProfileBtn.addEventListener("click", function() {profileMenu.style.display = "none";});
 
