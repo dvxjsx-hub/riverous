e8497b8cff9382138406d6cc2eaa0c5173d0e4d6
riverojsx <dvxjsx@gmail.com>
2026-09-05 19:54:55 -0500

    Update script.js

--- frontend/script.js
+++ frontend/script.js
@@ -1,3 +1,3 @@
 console.log("riverous iniciado");
 const enterbtn = document.getElementById("enter-btn");
-enterbtn.addEventListener("click", function() {document.getElementById("welcome").style.display = "none";});
+enterbtn.addEventListener("click", function() {document.getElementById("welcome").style.display = "none"; document.getElementById("home").style.display = "block";});
