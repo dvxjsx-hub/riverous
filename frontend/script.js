5aa4708531717c528364b9773451b5a8cb7f5d2c
riverojsx <dvxjsx@gmail.com>
2026-09-10 20:12:49 -0500

    Update script.js

--- frontend/script.js
+++ frontend/script.js
@@ -6,6 +6,7 @@ const profileGames = document.getElementById("profile-games");
 let currentGames = 0;
 
 enterBtn.addEventListener("click", function() {currentUser = usernameInput.value.trim(); if (currentUser === "") {currentUser = "JUGADOR";} profileUsername.textContent = currentUser; document.getElementById("welcome").style.display = "none"; document.getElementById("home").style.display = "block";});
+currentGames = currentGames + 1; profileGames.textContent = currentGames;
 
 const profileBtn = document.getElementById("profile-btn");
 const profileMenu = document.getElementById("profile-menu");
