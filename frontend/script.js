97e3551df5a14345bdb604590bc1aeba8ea8803f
riverojsx <dvxjsx@gmail.com>
2026-09-23 11:05:00 -0500

    Update script.js

--- frontend/script.js
+++ frontend/script.js
@@ -45,8 +45,8 @@ const playMenu = document.getElementById("play-menu");
 const menu = document.getElementById("menu");
 
 playBtn.addEventListener("click", function() {
-  menu.style.display = "none";
-  playMenu.style.display = "block";
+  mostrarPantalla(playMenu);
+  ocultarPantalla(menu);
 });
 
 // ==== PERFIL ====
